60cbc2e76ba07b810d7b7d30b4278808fcf035f7
Lucas Zampronio <lucas.zampronio.silva@gmail.com>
2026-05-26 23:12:13 -0300

    feat: music player

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Artist, Song, Playlist, UserProfile
 
-# Register your models here.
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre')
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'duration', 'has_audio')
+    
+    def has_audio(self, obj):
+        return bool(user_audio := obj.audio_file)
+    has_audio.boolean = True
+    has_audio.short_description = "Possui Áudio?"
+
+admin.site.register(Playlist)
+admin.site.register(UserProfile)
